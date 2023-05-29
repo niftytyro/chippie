743dbe61ebe0dc1da237a2795bde7196cf6e6f5f
Udasi Tharani <udasitharani@gmail.com>
2023-05-29 12:38:34 +0530

    refactor

--- main.c
+++ main.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void read_file(char path[]) {
+void read_rom(char path[]) {
   FILE *fptr;
 
   unsigned char command[2];
-  size_t bytes_read =0;
+  size_t bytes_read = 0;
 
   fptr = fopen(path, "rb");
 
   while ((bytes_read = fread(command, 1, sizeof(command), fptr))) {
-    printf("%x %x\n", command[0], command[1]);
+    printf("%x%x ", command[1], command[0]);
   }
 
   fclose(fptr);
 }
 
 int main() {
-  read_file("./test.ch8");
+  read_rom("./test.ch8");
   return 0;
 }
-
